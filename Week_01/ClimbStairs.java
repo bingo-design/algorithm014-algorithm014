de95a2b09bac910074df8be988a563ae1d2fbd32
jiabin1 <jiabin1@guazi.com>
2020-08-11 21:13:47 +0800

    add week 01

---
+++ Week_01/ClimbStairs.java
@@ -0,0 +1,20 @@
+class ClimbStairs {
+
+    /**
+     * 滚动数组法
+     * @param n
+     * @return
+     */
+    public int climbStairs(int n) {
+        int first = 0;
+        int second = 0;
+        int third = 1;
+        for (int i = 1; i <= n; i++) {
+            first = second;
+            second = third;
+            third = first + second;
+        }
+        return third;
+    }
+
+}
---
+++ Week_01/MaxArea.java
@@ -0,0 +1,41 @@
+public class MaxArea {
+
+    /**
+     * 暴力解法，循环嵌套
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        for (int i=0; i < height.length; i++) {
+            for (int j = height.length-1; j > 0; j--) {
+                if (i >= j) {
+                    break;
+                }
+                int h = Math.min(height[i], height[j]);
+                int w = j - i;
+                int area = h * w;
+                if (area > maxArea) {
+                    maxArea = area;
+                }
+            }
+        }
+        return maxArea;
+    }
+
+    /**
+     * 双指针法
+     * @param height
+     * @return
+     */
+    public static int maxArea2(int[] height) {
+        int i = 0, j = height.length - 1, res = 0;
+        while(i < j){
+            res = height[i] < height[j] ?
+                    Math.max(res, (j - i) * height[i++]):
+                    Math.max(res, (j - i) * height[j--]);
+        }
+        return res;
+    }
+
+}
